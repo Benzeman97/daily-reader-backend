778739ea04eec77bfafe3ea36b32418456dcf447
Benzeman97 <nafazbenzema@gmail.com>
2023-02-04 10:16:14 +0530

    update 1.0

--- src/main/java/net/dailyreader/model/NewsBody.java
+++ src/main/java/net/dailyreader/model/NewsBody.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-public class NewsBody {
+public class NewsBody implements Serializable {
 
     private int newsId;
     private String title;
--- src/main/java/net/dailyreader/model/TrendingNews.java
+++ src/main/java/net/dailyreader/model/TrendingNews.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
-public class TrendingNews {
+public class TrendingNews implements Serializable {
 
     private int newsId;
     private String title;
